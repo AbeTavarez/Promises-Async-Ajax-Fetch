17547eb8e8cd6bd38b3e274fc2b4064d79fbce20
abeTavarez <abeincwireless@gmail.com>
2021-09-03 15:10:47 -0400

    getting and creating posts sych

--- callbacks/app.js
+++ callbacks/app.js
@@ -4,9 +4,23 @@ const posts = [
     {title: 'Post Two', body: 'Post two body'}
 ];
 
-function createPost(post){
-    setTimeout(function(){
+const createPost = (post) => {
+    setTimeout(() =>{
         posts.push(post);
     },2000);
 };
 
+const getPosts = () => {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach(post => {
+            output += `<li>${post.title}</li>`
+        });
+        document.getElementById('app').innerHTML = output;
+    }, 1000);
+}
+
+const newPost = {title: 'New Post', body: 'This is a new post'}
+createPost({title: 'New Post', body: 'This is a new post'})
+
+getPosts();

996d4ccf74da5827b87f3bd905d08e09f5a49465
abeTavarez <abeincwireless@gmail.com>
2021-09-02 22:48:56 -0400

    dhowing customers data on UI

--- ajax-json/app.js
+++ ajax-json/app.js
@@ -8,6 +8,8 @@ const btnTwo = document.getElementById('button2');
 
 
 //* ============ FUNCTIONS
+
+// Load single Customer
 const loadCustomer = () => {
     const xhr = new XMLHttpRequest();
 
@@ -27,7 +29,7 @@ const loadCustomer = () => {
                 </ul>
             `;
 
-            document.getElementById('customer').innerHTML = output
+            document.getElementById('customer').innerHTML = output;
             
             // console.log(xhr.responseText);
         }
@@ -36,8 +38,44 @@ const loadCustomer = () => {
     xhr.send()
 };
 
+// Load all customers
+const loadCustomers = () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customers.json', true);
+
+    xhr.onload = () => {
+        if (xhr.status === 200){
+            // parse JSON to an object
+            const customers = JSON.parse(xhr.responseText);
+            // create empty string variable
+            let output = ''; // HTML needs to be strings to add it to innerHTML
+            // loop over the customers array
+            customers.forEach(customer => {
+                // foreach customer create these elements and append it to output
+                output += `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Company: ${customer.company}</li>
+                    <li>Phone: ${customer.phone}</li>
+                </ul>
+            `;
+            //console.log(output);
+            });
+
+            document.getElementById('customers').innerHTML = output;
+        }
+    }
+
+    xhr.send();
+}
+
+
 
 //* ==================== Event Listeners
 
 btnOne.addEventListener('click', loadCustomer);
+
+btnTwo.addEventListener('click', loadCustomers);
 

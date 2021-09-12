7b7946dcbfd2fb452cb922dee05db5c8d320d30c
abeTavarez <abeincwireless@gmail.com>
2021-09-11 23:22:33 -0400

    getting data

---
+++ easy-http/easy-http.js
@@ -0,0 +1,26 @@
+
+//* ========= ES5
+
+function EasyHTTP() {
+    this.http = new XMLHttpRequest();
+}
+
+//* Make a HTTP GET Request
+EasyHTTP.prototype.get = function(url){
+    this.http.open('GET', url, true);
+
+    let self = this; // capture 'this' on this scope
+
+    this.http.onload = function() {
+        if (self.http.status === 200){
+            console.log(self.http.responseText);
+        }
+    }
+
+    this.http.send();
+}
+
+//* Make a HTTP GET Request
+//* Make a HTTP GET Request
+//* Make a HTTP GET Request
+//* Make a HTTP GET Request

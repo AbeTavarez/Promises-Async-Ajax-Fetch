9c655f5c0235dabc5bd619a36a662195451c4b58
abeTavarez <abeincwireless@gmail.com>
2021-09-19 21:07:18 -0400

    get single post

--- easy-http/app.js
+++ easy-http/app.js
@@ -2,10 +2,19 @@
 const http = new EasyHTTP();
 
 //* Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+//     if (error) {
+//         console.error(error)
+//     } else {
+//         console.log(posts);
+//     }
+//   });
+
+//* Get Single Post
+http.get('https://jsonplaceholder.typicode.com/posts/1', function (error, post) {
     if (error) {
         console.error(error)
     } else {
-        console.log(posts);
+        console.log(post);
     }
-  })
+  });
